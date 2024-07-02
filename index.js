a3b771e38ccbbb3415b522f643577fefffe26c57
victorndungi <victor.ndungi@student.moringaschool.com>
2024-07-02 11:53:32 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(pick){
-   const headquaters=42
-   return pick-headquaters
+   let headquaters=42
+   if (pick > headquaters){
+    let actualDistance = pick-headquaters
+     return actualDistance
+   }else if (pick < headquaters) { 
+    return headquaters - pick
+   }else {
+    return 'in the same block'
+   }
 }
 function distanceFromHqInFeet(pick){
-    distanceFromHqInBlocks(pick)
-    const feet = 264
-    return pick*feet
+    let destination;
+    destination = distanceFromHqInBlocks(pick)*264
+    return destination
 }
  function distanceTravelledInFeet(start, destination){
- const blockstravelled=(destination-start)
- const feetperblock= 264
- return blockstravelled*feetperblock
+//  const distanceFromHqInFeet= Math.abs(destination-start)*264
+//  return distanceFromHqInFeet
+if (start < destination){
+    return (destination - start)*264
+}else {
+    return (start - destination)*264
+}
+ 
  }
  
 
 function calculatesFarePrice(start, destination){
-    const distance = distanceTravelledInFeet (start, destination)
+    const distance = distanceTravelledInFeet(start, destination)
     if (distance<=400){
         return 0
     }else if (distance > 400 && distance <= 2000){
-        return (distance - 0)*0.02
-    }else if (distance >2000 && distance <= 2500){
+        return (distance - 400)*0.02
+    }else if (distance >2000 && distance < 2500){
         return 25
     }else{
         return 'cannot travel that far'
 }
 
 }
-console.log(distanceFromHqInBlocks(50))
-console.log(distanceTravelledInFeet(10, 60))
+console.log(distanceFromHqInBlocks(30))
+console.log(distanceTravelledInFeet(4, 60))
 console.log(distanceFromHqInFeet(50))
-console.log(calculatesFarePrice(40, 60))
+console.log(calculatesFarePrice(0, 60))
 
